077708c71a100539ecf4a4292747263a5da4004d
Windar Shineta <newwinda9@gmail.com>
2026-05-08 21:27:30 +0700

    perbaikan struktur folder, alur navigasi

--- config/login.php
+++ config/login.php
@@ -7,31 +7,12 @@ if (isset($_POST['login'])) {
     $password = $_POST['password'];
 
     $query = mysqli_query($conn, "SELECT * FROM users WHERE username='$username' AND password='$password'");
-    $cek = mysqli_num_rows($query);
-
-    if ($cek > 0) {
-        $_SESSION['username'] = $username;
+    
+    if (mysqli_num_rows($query) > 0) {
         $_SESSION['status'] = "login";
-        header("location:dashboard.php");
+        header("location:dashboard.php"); // Pindah ke dashboard otomatis
     } else {
-        header("location:login.php?pesan=gagal");
+        echo "Gagal Login!";
     }
 }
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Login - Katalog Sastra</title>
-    <link rel="stylesheet" href="assets/style.css">
-</head>
-<body>
-    <div class="container">
-        <h2>Login Resensi Buku</h2>
-        <form method="POST" action="">
-            <input type="text" name="username" placeholder="Username" required><br>
-            <input type="password" name="password" placeholder="Password" required><br>
-            <button type="submit" name="login">Masuk</button>
-        </form>
-    </div>
-</body>
-</html>
