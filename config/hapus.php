0fa0a8db71cb1501f9baa00c18b9989f0b2d33c8
Windar Shineta <newwinda9@gmail.com>
2026-05-09 07:54:14 +0700

    revisi seluruh alur dan merapikan struktur folder

--- config/hapus.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'config/koneksi.php';
-$id = $_GET['id'];
-mysqli_query($conn, "DELETE FROM buku WHERE id=$id");
-header("Location: index.php");
-?>
--- config/koneksi.php
+++ config/koneksi.php
@@ -2,11 +2,11 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db   = "uts_web"; 
+$db   = "uts_web";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
-    die("Koneksi ke database gagal: " . mysqli_connect_error());
+    die("Koneksi gagal: " . mysqli_connect_error());
 }
 ?>
