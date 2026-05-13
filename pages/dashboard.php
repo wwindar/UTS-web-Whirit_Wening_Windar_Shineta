afde65be1e814983ee33554c4338f99c672a5cb5
Windar Shineta <newwinda9@gmail.com>
2026-05-13 14:07:30 +0700

    revisi

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -117,9 +117,6 @@ function renderStars($rating) {
         </div>
         <?php include '../includes/footer.php'; ?>
 
-    <footer class="footer">
-        <p>© <?= date('Y') ?> Katalog Resensi Buku &nbsp;·&nbsp; UTS Praktikum Web Windar Shineta</p>
-    </footer>
     <script src="<?= $basePath ?? '../' ?>assets/js/main.js"></script>
 </body>
 </html>
